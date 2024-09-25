2dcbee9054e7dca1ceb5470091e640eef8e5a824
Skyla Miranda Reyes <skylamiranda643@gmail.com>
2024-09-24 20:49:49 -0400

    demos!

---
+++ python demo 2.py
@@ -0,0 +1,12 @@
+class Dog:
+    def __init__(self, name, age)
+        self.name = name
+        self.age = age
+    def bark(self): 
+        print("Woof!")
+        def get_details(self):
+            return f"name": {self.name}, age: {self.age}
+
+my_dog = dog("chitto" , age:5)
+my_dog.bark()
+print(my_dog.get_details())
