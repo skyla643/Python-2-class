2bac698d1c07938b90a3f5bd6df7c87a65aab07f
Skyla Miranda Reyes <skylamiranda643@gmail.com>
2024-10-01 20:17:07 -0400

    eh eh ehhh

---
+++ python demo.py
@@ -0,0 +1,28 @@
+# create an empty stack
+
+#push items into the stack
+def push(value):
+    stack.append(value)
+    print(f"pushed: {value}")
+
+# pop  an item from the stack
+def pop():
+    if is_empty():
+        print("Stack is empty, can't stop")
+else:
+    print(f"popped: {item}")
+
+# check if stack is empty
+def is_empty():
+    if is_empty():
+        print("stack is empty, nothing is peak")
+        else:
+print(f"top item: {stack[-1]}")
+
+push(10)
+push(20)
+push(30)
+
+peek( )
+peek( )
+
